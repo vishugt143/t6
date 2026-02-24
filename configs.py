084cee9bf68b96aafd58a8b2e67bc7d797e2d70b
vishugt143 <158712030+vishugt143@users.noreply.github.com>
2026-02-24 12:29:21 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -18,8 +18,8 @@ class Config:
 
     # Posts to copy
     POSTS = [
-        "https://t.me/forward_hack_lnx/190",
         "https://t.me/forward_hack_lnx/197",
+        "https://t.me/forward_hack_lnx/190",
         "https://t.me/forward_hack_lnx/25"
     ]
 
